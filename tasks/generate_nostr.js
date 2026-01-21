b47f5e2b709b1ab1b744fded1220aec8cb8e2a34
Dennis Reimann <mail@dennisreimann.de>
2026-01-21 08:40:09 +0100

    Add NIP-05 handles for Vereinsmitglieders :D

--- tasks/generate_nostr.js
+++ tasks/generate_nostr.js
@@ -1,8 +1,17 @@
 const { mkdirSync, writeFileSync } = require('fs')
 const { dirname, resolve } = require('path')
 const { NDKUser } = require('@nostr-dev-kit/ndk')
+const request = require('sync-request')
 const participants = require('../content/participants.json')
 const { npub } = require('../content/meta.json')
+
+let members = []
+try {
+  const jsonBody = request('GET', 'https://verein.einundzwanzig.space/api/members/2026').getBody('utf8')
+  members = JSON.parse(jsonBody)
+} catch (err) {
+  console.error('Could not load members:', err)
+}
 
 const einundzwanzig = new NDKUser({ npub: npub.einundzwanzig })
 const nostrtalk = new NDKUser({ npub: npub.nostrTalk })
@@ -24,6 +33,15 @@ Object.entries(participants).forEach(([key, { nostr: npub }]) => {
   const id = key.replace(/[\s]/g, '_')
   names[id] = new NDKUser({ npub }).pubkey
 })
+
+Object.entries(members).forEach(([key, { npub, pubkey, nip05_handle }]) => {
+  if (!nip05_handle) return
+  const id = nip05_handle.replace(/[\s]/g, '_')
+  // don't overwrite participant entry
+  if (names[id]) return
+  names[id] = new NDKUser({ npub }).pubkey
+})
+
 
 const dst = resolve(__dirname, '..', 'dist', '.well-known', 'nostr.json')
 const dir = dirname(dst)
